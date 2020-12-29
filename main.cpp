74bda3e0c36baa0b501c179b15a53b0f94245c2a
Alexandr Lazarev <47700257+7841VC@users.noreply.github.com>
2020-12-29 11:00:10 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include"Site.h"
 
 /*
 Создать систему запросов «Сайт → Магазины (3 штуки) → Склад». 
@@ -11,8 +12,42 @@ using namespace std;
 int main(void)
 {
 	setlocale(LC_ALL, "rus");
-
+	Site* s = new Site();
+	s->createSite();
+	s->getSite();
+	s->saveSite();
+	int ans;
+	while (1)
+	{
+		
+			cout << "Выберите операцию: " << endl;
+			cout << "|1| - Вывести сайт" << endl;
+			cout << "|2| - Получить интересующий путеводитель" << endl;
+			cout << "|3| - выйти из меню покупателя" << endl;
+			cin >> ans;
+			switch (ans)
+			{
+				case 1:
+				{
+					s->getSite();
+					break;
+				}
+				case 2:
+				{
+					cout << "Введите артикль путеводителя: " << endl;
+					cin >> ans;
+					travelGuide* tmp = s->findGuide(ans);
+					cout << "С приобритением путеводителя: " << endl;
+					tmp->getGuide();
+					break;
+				}
+				
+			}
+			if (ans == 3) { break; }
+		
+	}
 
 	cin.get(); cin.get();
+	delete s;
 	return 0;
 }
